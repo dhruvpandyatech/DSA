d06b748eb71521a67b3108100f4f1d62ab6f3c74
Dhruv Pandya <dhruvpandya.tech@gmail.com>
2026-08-29 17:39:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Iterative Inorder/iterative-inorder.cpp
@@ -0,0 +1,44 @@
+/*
+Definition for Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    vector<int> inOrder(Node* root) {
+        // code here
+        stack<Node*> st;
+        Node* node = root;
+        vector<int> inorder;
+        
+        while(true)
+        {
+            if(node!=NULL)
+            {
+                st.push(node);
+                node = node->left;
+            }
+            else
+            {
+                if(st.empty() == true)
+                {
+                    break;
+                }
+                node = st.top();
+                st.pop();
+                inorder.push_back(node->data);
+                node = node->right;
+            }
+        }
+        return inorder;
+    }
+};
